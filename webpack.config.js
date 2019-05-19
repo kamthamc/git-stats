ed3dd37c8c2200adc4c09d0c924fa329050aabed
Chaitanya <kamthamc@users.noreply.github.com>
2019-05-18 19:02:11 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: process.env.PORT || 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.css$/,
+                use: 'css-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: require('html-webpack-template'),
+            inject: false,
+            appMountId: 'app',
+            lang:'en-us',
+            title: 'Git Stats',
+            meta: [
+                {
+                    name: 'viewport',
+                    width: 'device-width',
+                    'initial-scale': 1,
+                    'user-scalable': 'no',
+                }
+            ]
+        })
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\\/]node_modules[\\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+};
+
+module.exports = config;
